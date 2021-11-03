7daa4b8fe0de6236829814d0280ead8160f37a12
Jefferson Vieira França <67725540+Jeffvf@users.noreply.github.com>
2021-11-02 21:26:35 -0300

    Add files via upload

---
+++ jogo_da_vida.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<omp.h>
+
+int getNeighbors(int** grid, int i, int j) {
+
+}
+
+
+int main(){
+    int r = 2048, c = 2048, i, j, count;
+ 
+    int** grid = (int**)malloc(r * sizeof(int*));
+    for (i = 0; i < r; i++)
+        grid[i] = (int*)malloc(c * sizeof(int));
+  
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            grid[i][j] = 0;
+
+    //GLIDER
+    int lin = 1, col = 1;
+    grid[lin  ][col+1] = 1;
+    grid[lin+1][col+2] = 1;
+    grid[lin+2][col  ] = 1;
+    grid[lin+2][col+1] = 1;
+    grid[lin+2][col+2] = 1;
+    
+    //R-pentomino
+    lin =10; col = 30;
+    grid[lin  ][col+1] = 1;
+    grid[lin  ][col+2] = 1;
+    grid[lin+1][col  ] = 1;
+    grid[lin+1][col+1] = 1;
+    grid[lin+2][col+1] = 1;
+
+}
