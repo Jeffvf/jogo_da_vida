d1b46d4d37a8791bdca1ea7756584dac3d366776
Jefferson Vieira <jefferson.vieiraf@hotmail.com>
2021-11-03 20:34:02 -0300

    Contagem de vizinhos
    Adicionada função getNeighbors que conta os vizinhos de uma célula dada.

--- jogo_da_vida.c
+++ jogo_da_vida.c
@@ -1,9 +1,51 @@
 #include<stdio.h>
 #include<pthread.h>
+#include<stdlib.h>
 #include<omp.h>
 
+int r = 50, c = 50;
+
+int getNeighbors(int** grid, int lin, int col) {
+    int count = 0, column, l, i, j;
+  
+    for(i=-1; i<2; i++){
+        
+        column = col;
+        l = lin;
+      
+        if (lin + i < 0){
+            l = r;
+        }
+
+        else if (lin + i == r){
+            l = 0;
+        }
+
+        for(j=-1; j<2; j++){
+
+            if (col + j < 0){
+                column = c;
+            }
+            
+            else if (col + j == c){
+                column = 0;
+            }
+
+            if (i == 0 && j == 0){
+                
+            }
+            else{
+                count += grid[l+i][column+j];
+            }
+        }
+    }
+
+    return count;
+}
+
+
 int main(){
-    int r = 2048, c = 2048, i, j, count;
+    int i, j, count, neighbors;
  
     int** grid = (int**)malloc(r * sizeof(int*));
     for (i = 0; i < r; i++)
@@ -28,5 +70,10 @@ int main(){
     grid[lin+1][col  ] = 1;
     grid[lin+1][col+1] = 1;
     grid[lin+2][col+1] = 1;
+
+    neighbors = getNeighbors(grid, 10,30);
+    printf("%d\n", neighbors);
+
+    return 0;
 
 }
